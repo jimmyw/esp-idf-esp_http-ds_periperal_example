e3e582c71434f42af9bcda63e4ef9e0e54a13991
Jimmy Wennlund <jimmy@wennlund.nu>
2023-10-18 21:56:49 +0200

    Remove mqtt stuff

--- main/app_main.c
+++ main/app_main.c
@@ -26,7 +26,6 @@
 #include "lwip/netdb.h"
 
 #include "esp_log.h"
-#include "mqtt_client.h"
 #include "rsa_sign_alt.h"
 #include "esp_secure_cert_read.h"
 #include "esp_http_client.h"
@@ -39,110 +38,12 @@
 #define MAX(a, b) (((a) > (b)) ? (a) : (b))
 #define STRLEN(str) (sizeof(str) - 1)
 
-static const char *TAG = "MQTTS_EXAMPLE";
+static const char *TAG = "DS_HTTPS_EXAMPLE";
 
 #define MAX_HTTP_RECV_BUFFER 512
 #define MAX_HTTP_OUTPUT_BUFFER 2048
 
-#if 0
-extern const uint8_t server_cert_pem_start[] asm("_binary_mosquitto_org_crt_start");
-extern const uint8_t server_cert_pem_end[] asm("_binary_mosquitto_org_crt_end");
 
-/*
- * @brief Event handler registered to receive MQTT events
- *
- *  This function is called by the MQTT client event loop.
- *
- * @param handler_args user data registered to the event.
- * @param base Event base for the handler(always MQTT Base in this example).
- * @param event_id The id for the received event.
- * @param event_data The data for the event, esp_mqtt_event_handle_t.
- */
-static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
-{
-    ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%" PRIi32, base, event_id);
-    esp_mqtt_event_handle_t event = event_data;
-    esp_mqtt_client_handle_t client = event->client;
-    int msg_id;
-    // your_context_t *context = event->context;
-    switch ((esp_mqtt_event_id_t)event_id) {
-    case MQTT_EVENT_CONNECTED:
-        ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
-        msg_id = esp_mqtt_client_subscribe(client, "/topic/qos0", 0);
-        ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
-
-        msg_id = esp_mqtt_client_subscribe(client, "/topic/qos1", 1);
-        ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
-
-        msg_id = esp_mqtt_client_unsubscribe(client, "/topic/qos1");
-        ESP_LOGI(TAG, "sent unsubscribe successful, msg_id=%d", msg_id);
-        break;
-    case MQTT_EVENT_DISCONNECTED:
-        ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
-        break;
-
-    case MQTT_EVENT_SUBSCRIBED:
-        ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
-        msg_id = esp_mqtt_client_publish(client, "/topic/qos0", "data", 0, 0, 0);
-        ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
-        break;
-    case MQTT_EVENT_UNSUBSCRIBED:
-        ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
-        break;
-    case MQTT_EVENT_PUBLISHED:
-        ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
-        break;
-    case MQTT_EVENT_DATA:
-        ESP_LOGI(TAG, "MQTT_EVENT_DATA");
-        printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
-        printf("DATA=%.*s\r\n", event->data_len, event->data);
-        break;
-    case MQTT_EVENT_ERROR:
-        ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
-        break;
-    default:
-        ESP_LOGI(TAG, "Other event id:%d", event->event_id);
-        break;
-    }
-}
-
-static void mqtt_app_start(void)
-{
-    /* The context is used by the DS peripheral, should not be freed */
-    esp_ds_data_ctx_t *ds_data = esp_secure_cert_get_ds_ctx();
-    if (ds_data == NULL) {
-        ESP_LOGE(TAG, "Error in reading DS data from NVS");
-        vTaskDelete(NULL);
-    }
-    char *device_cert = NULL;
-    esp_err_t ret;
-    uint32_t len;
-    ret = esp_secure_cert_get_device_cert(&device_cert, &len);
-    if (ret != ESP_OK) {
-        ESP_LOGE(TAG, "Failed to obtain the device certificate");
-        vTaskDelete(NULL);
-    }
-
-    const esp_mqtt_client_config_t mqtt_cfg = {
-        .broker = {
-            .address.uri = "mqtts://test.mosquitto.org:8884",
-            .verification.certificate =  (const char *)server_cert_pem_start,
-        },
-        .credentials = {
-            .authentication = {
-                .certificate = (const char *)device_cert,
-                .key = NULL,
-                .ds_data = (void *)ds_data
-            },
-        },
-    };
-
-    ESP_LOGI(TAG, "[APP] Free memory: %" PRIu32 " bytes", esp_get_free_heap_size());
-    esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
-    esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
-    esp_mqtt_client_start(client);
-}
-#endif
 
 esp_err_t _http_event_handler(esp_http_client_event_t *evt)
 {
